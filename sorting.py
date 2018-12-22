282d3b56e9768b7431ca92e782ea91b9dff78326
eric <mahasieric@gmail.com>
2018-12-22 10:30:00 +0000

    Minimal refactoring and added TODO items.

--- sorting.py
+++ sorting.py
@@ -1,30 +1,29 @@
-""" Python script to implement some simple sorting algorithms.
-    I will handle the bubble, insertion and merge sort algorithms."""
+""" Python script to implement some classic sorting algorithms; bubble, insertion and merge sort algorithms."""
 import random
 
 
 class Sorting(object):
-    ''' constructor creates new data on every instance of the class.'''
+    """Constructor creates new data on every instance of the class."""
 
     def __init__(self):
-        self.numbers = []
+        self.numbers_list = []
         # List comprehension to generate integers 1 through 10.
         self.data = [x for x in range(1, 11)]
         random.shuffle(self.data)
-        # Place those randomly ordered numbers in a list.
-        self.numbers = self.data
-        print(self.numbers)
+        self.numbers_list = self.data
+        print(self.numbers_list)
    
     def bubble_sort(self):
-        # Nested for loops to go through all elements.
-        for i in range(0, len(self.numbers) - 1):
-            for j in range(0, len(self.numbers) - 1 - i):
-                if self.numbers[j] > self.numbers[j + 1]:
+        for i in range(0, len(self.numbers_list) - 1):
+            for j in range(0, len(self.numbers_list) - 1 - i):
+                if self.numbers_list[j] > self.numbers_list[j + 1]:
                     # Swap operation.
-                    self.numbers[j], self.numbers[j + 1] = self.numbers[j + 1], self.numbers[j]
-        return self.numbers
+                    self.numbers_list[j], self.numbers_list[j + 1] = self.numbers_list[j + 1], self.numbers_list[j]
+        return self.numbers_list
+# TODO add insertion and merge sort algorithms
+# TODO choose better variable name for data variable
 
 
 a = Sorting()
 a.bubble_sort()
-print(a.numbers)
+print(a.numbers_list)
