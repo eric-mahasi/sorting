c936eca383dfc9ced541881effc931995b8bc096
eric <mahasieric@gmail.com>
2018-12-25 09:14:58 +0000

    Added a function that allows more functionality; specifying how many numbers they want sorted as well as inputting as many numbers as they specified. Cleaned up the init function. Added docstrings for bubble and insertion sort algorithms. Cleared four items on TODO list.

--- sorting.py
+++ sorting.py
@@ -5,13 +5,25 @@ import random
 class Sorting(object):
 
     def __init__(self):
-        """Constructor produces a new list of shuffled integers for every object."""
         self.swapped = False
-        self.numbers_list = [x for x in range(1, 11)]  # List comprehension to generate integers 1 through 10.
-        random.shuffle(self.numbers_list)
-        print(self.numbers_list)
+        self.numbers_list = []
+        self.insertion_position = 0
+        self.value_to_insert = 0
+
+    def get_user_input(self):
+        """This function allows the user to specify how many and which integers they would like to be sorted. Returns
+        a list of randomly arranged integers provided by the user"""
+        n = int(input("How many numbers would you like sorted? "))
+        for i in range(n):
+            print("Enter number", i + 1)
+            number = int(input())
+            self.numbers_list.append(number)
+            random.shuffle(self.numbers_list)
+        return self.numbers_list
 
     def bubble_sort(self):
+        """This sorting algorithm is a comparison-based algorithm in which each
+        pair of adjacent elements is compared and the elements are swapped if they are not in order."""
         for i in range(len(self.numbers_list) - 1):
             for j in range(len(self.numbers_list) - 1 - i):
                 if self.numbers_list[j] > self.numbers_list[j + 1]:
@@ -23,25 +35,27 @@ class Sorting(object):
         return self.numbers_list
 
     def insertion_sort(self):
+        """This is an in-place comparison-based sorting algorithm. Here, a sub-list is maintained which is always sorted.
+        For example, the lower part of an array is maintained to be sorted. An element which is to be 'insert'ed in this
+        sorted sub-list, has to find its appropriate place and then it has to be inserted there. Hence the name,
+        insertion sort"""
         for i in range(len(self.numbers_list) - 1):
             self.insertion_position = i
             self.value_to_insert = self.numbers_list[i]
             while self.insertion_position > 0 and self.numbers_list[self.insertion_position - 1] > self.value_to_insert:
                 self.insertion_position -= 1
             self.numbers_list[self.insertion_position] = self.value_to_insert
+        return self.numbers_list
 
     def merge_sort(self):
         pass
 
 # TODO add merge sort algorithm
-# TODO add functionality for numbers list to contain as many integers as necessary
-# TODO allow user to choose which numbers they would like sorted
-# TODO add some print statements to clarify output
-# TODO add some documentation to explain how functions work
 
 
+# Objects for debugging purposes
 a = Sorting()
+print("Original list:", a.get_user_input())
 a.bubble_sort()
-a.insertion_sort()
-print(a.numbers_list)
-print(a.numbers_list)
+# a.insertion_sort()
+print("Sorted list:", a.numbers_list)
