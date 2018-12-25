4d6f4c26c5a15d702227c6ef37240232ffa94db9
eric <mahasieric@gmail.com>
2018-12-25 09:16:23 +0000

    Minor refactoring.

--- sorting.py
+++ sorting.py
@@ -12,7 +12,7 @@ class Sorting(object):
 
     def get_user_input(self):
         """This function allows the user to specify how many and which integers they would like to be sorted. Returns
-        a list of randomly arranged integers provided by the user"""
+        a list of randomly arranged integers provided by the user."""
         n = int(input("How many numbers would you like sorted? "))
         for i in range(n):
             print("Enter number", i + 1)
@@ -23,7 +23,7 @@ class Sorting(object):
 
     def bubble_sort(self):
         """This sorting algorithm is a comparison-based algorithm in which each
-        pair of adjacent elements is compared and the elements are swapped if they are not in order."""
+        pair of adjacent elements is compared and the elements are swapped if they are not in order. Returns a sorted list."""
         for i in range(len(self.numbers_list) - 1):
             for j in range(len(self.numbers_list) - 1 - i):
                 if self.numbers_list[j] > self.numbers_list[j + 1]:
@@ -38,7 +38,7 @@ class Sorting(object):
         """This is an in-place comparison-based sorting algorithm. Here, a sub-list is maintained which is always sorted.
         For example, the lower part of an array is maintained to be sorted. An element which is to be 'insert'ed in this
         sorted sub-list, has to find its appropriate place and then it has to be inserted there. Hence the name,
-        insertion sort"""
+        insertion sort. Returns a sorted list."""
         for i in range(len(self.numbers_list) - 1):
             self.insertion_position = i
             self.value_to_insert = self.numbers_list[i]
@@ -49,6 +49,7 @@ class Sorting(object):
 
     def merge_sort(self):
         pass
+
 
 # TODO add merge sort algorithm
 
